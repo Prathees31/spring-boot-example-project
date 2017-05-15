900b2fdada7946db75023471e98e6ba3457e1ad4
Prathees31 <prathees.g.s@gmail.com>
2017-05-15 22:09:00 +0530

    first Commit

---
+++ src/main/java/com/pratheesgsweb/EcommercestoreApplication.java
@@ -0,0 +1,12 @@
+package com.pratheesgsweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercestoreApplication.class, args);
+	}
+}
